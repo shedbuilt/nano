b8e2ceb8e23ff43a7bde337755262ffcd6589fc9
Auston Stewart <auston@shedbuilt.net>
2017-12-19 11:08:26 -1000

    Updating contrib packaging.

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ make "DESTDIR=${SHED_FAKEROOT}" install
 mkdir -pv "${SHED_FAKEROOT}/usr/share/doc/nano-2.8.7"
 install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKEROOT}/usr/share/doc/nano-2.8.7"
 mkdir -v "${SHED_FAKEROOT}/etc"
-install -v -m644 "${SHED_CONTRIBDIR}/nanorc.default" "${SHED_FAKEROOT}/etc"
+install -v -m644 "${SHED_CONTRIBDIR}/nanorc" "${SHED_FAKEROOT}/etc/nanorc.default"
