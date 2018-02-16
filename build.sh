bdeaa333afbb73897affff4962da331b63113752
Auston Stewart <auston@shedbuilt.net>
2018-02-15 23:19:32 -1000

    Updating to 2.9.3

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 ./configure --prefix=/usr     \
             --sysconfdir=/etc \
             --enable-utf8     \
-            --docdir=/usr/share/doc/nano-2.8.7
-make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
-mkdir -pv "${SHED_FAKEROOT}/usr/share/doc/nano-2.8.7"
-install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKEROOT}/usr/share/doc/nano-2.8.7"
-mkdir -v "${SHED_FAKEROOT}/etc"
-install -v -m644 "${SHED_CONTRIBDIR}/nanorc" "${SHED_FAKEROOT}/etc/nanorc.default"
+            --docdir=/usr/share/doc/nano-2.9.3 && \
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
+mkdir -pv "${SHED_FAKEROOT}/usr/share/doc/nano-2.9.3"
+install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKEROOT}/usr/share/doc/nano-2.9.3"
+install -v -Dm644 "${SHED_CONTRIBDIR}/nanorc" "${SHED_FAKEROOT}/etc/nanorc.default"
