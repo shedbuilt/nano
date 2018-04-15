a5c21e95170be5325d2dc8bfe8e1ee9a5f6de566
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:19:26 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
             --sysconfdir=/etc \
             --enable-utf8     \
             --docdir=/usr/share/doc/nano-2.9.3 && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-mkdir -pv "${SHED_FAKEROOT}/usr/share/doc/nano-2.9.3"
-install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKEROOT}/usr/share/doc/nano-2.9.3"
-install -v -Dm644 "${SHED_CONTRIBDIR}/nanorc" "${SHED_FAKEROOT}/etc/nanorc.default"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+mkdir -pv "${SHED_FAKE_ROOT}/usr/share/doc/nano-2.9.3"
+install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKE_ROOT}/usr/share/doc/nano-2.9.3"
+install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/nanorc" "${SHED_FAKE_ROOT}/etc/nanorc.default"
