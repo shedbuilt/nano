dc0e306dc1b433306762f766f97d0bd3f92713af
shedbuilt-jb <jb@shedbuilt.net>
2018-09-16 19:55:53 -0400

    fix changed variables for blank

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
             --enable-utf8     \
             --docdir="$SHED_PKG_DOCS_INSTALL_DIR" && \
 make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-mkdir -pv "${SHED_FAKEROOT}${$SHED_PKG_DOCS_INSTALL_DIR}"
-install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKEROOT}${$SHED_PKG_DOCS_INSTALL_DIR}"
-install -v -Dm644 "${SHED_CONTRIBDIR}/nanorc" "${SHED_FAKEROOT}/etc/nanorc.default"
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+mkdir -pv "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
+install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
+install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/nanorc" "${SHED_FAKE_ROOT}/etc/nanorc.default"
