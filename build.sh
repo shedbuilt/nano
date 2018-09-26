0acf2f0f4b2bb2ccefd370c9576780717dd0ec5b
Auston Stewart <auston@shedbuilt.net>
2018-09-25 16:26:42 -1000

    Using automatic defaults installation for nanorc

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install &&
 mkdir -pv "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}" &&
 install -v -m644 doc/{nano.html,sample.nanorc} "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}" &&
-install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/nanorc" "${SHED_FAKE_ROOT}/etc/nanorc.default"
+install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/nanorc" "${SHED_FAKE_ROOT}${SHED_PKG_DEFAULTS_INSTALL_DIR}/etc/nanorc"
